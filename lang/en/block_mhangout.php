c28b0013de3719f664e759646c518c326573aa81
Ronnald R. Machado <rrmachado@id.uff.br>
2017-03-29 11:12:41 -0300

    First Commit

---
+++ lang/en/block_mhangout.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_mhangout', language 'en'
+ *
+ * @package   block_mhangout
+ * @copyright Ronnald Machado <rrmachado@id.uff.br>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['blockstring'] = 'Block string';
+$string['descconfig'] = 'Description of the config section';
+$string['descfoo'] = 'Config description';
+$string['headerconfig'] = 'Config section header';
+$string['labelfoo'] = 'Config label';
+$string['mhangout:addinstance'] = 'Add a mhangout block';
+$string['mhangout:myaddinstance'] = 'Add a mhangout block to my moodle';
+$string['pluginname'] = 'mhangout';
